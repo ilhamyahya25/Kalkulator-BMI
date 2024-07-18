95af2ec59fc3d6d64d5a9b9a04ba7868f808755f
ilham yahya mulyana <ilhamyahyamulyana25@gmail.com>
2024-07-18 10:10:08 +0700

    update

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,33 @@
-//* ini javascript *//
+function calculateBMI() {
+    const weight = parseFloat(document.getElementById('weight').value);
+    const height = parseFloat(document.getElementById('height').value) / 100;
+
+    if (isNaN(weight) || isNaN(height)) {
+        document.getElementById('result').innerText = 'Harap masukkan nilai yang valid.';
+        return;
+    }
+
+    const bmi = (weight / (height * height)).toFixed(1);
+    let category = '';
+
+    if (bmi < 18.5) {
+        category = 'Kurus';
+    } else if (bmi >= 18.5 && bmi <= 24.9) {
+        category = 'Ideal';
+    } else {
+        category = 'Gemuk';
+    }
+
+    document.getElementById('result').innerHTML = `
+        <p>Berat Badan: ${bmi}</p>
+        <p>Anda memiliki tubuh ${category}</p>
+    `;
+}
+
+function resetForm() {
+    document.getElementById('gender').value = 'male';
+    document.getElementById('weight').value = '';
+    document.getElementById('age').value = '';
+    document.getElementById('height').value = '';
+    document.getElementById('result').innerText = '';
+}
